be2fe1c09d29df9126e23802d0bb55aeb4c93a93
Lars Bärtschi <lars.baertschi@bluewin.ch>
2018-10-23 23:06:37 +0200

    Add insertion to text box, add querying of support forms

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,3 +1,7 @@
+function log(msg) {
+	console.log("[%cWrenchie%c] %s","color: orange","", msg);
+}
+
 chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
         if (document.readyState === "complete") {
@@ -10,6 +14,38 @@ chrome.extension.sendMessage({}, function(response) {
                 "| | /| / / /_/ / __/ /  |/ / /   / /_/ // // __/   \n" +
                 "| |/ |/ / _, _/ /___/ /|  / /___/ __  // // /___   \n" +
                 "|__/|__/_/ |_/_____/_/ |_/\\____/_/ /_/___/_____/   Wrenchie " + chrome.runtime.getManifest().version + " was loaded.\n");
+
+						// Look up if there is a textarea
+						const mainComment = $("#comments > div.reply.form-container.glow.box.secondary > div.textarea-holder > div > textarea");
+
+						if (mainComment.length === 1) {
+							log("Main comment textarea found.");
+							insertFormat(mainComment);
+						}
         }
 	}, 10);
 });
+
+function insertFormat(textarea) {
+	chrome.storage.sync.get(["settings_format"], function(value) {
+		let format = value.settings_format;
+		let cursorPosition = -1;
+
+		if (format.includes("%CURSOR%")) {
+            cursorPosition = format.indexOf("%CURSOR%");
+
+            format = format.replace("%CURSOR%", "");
+        }
+
+		textarea.val(format);
+		textarea[0].dispatchEvent(new Event("change")); // it has to be done like this resolving textarea again
+
+        if (cursorPosition > 0) {
+            // Place cursor
+            textarea[0].focus();
+            textarea[0].setSelectionRange(cursorPosition, cursorPosition);
+        }
+
+		log("Format inserted into textarea. Cursor placed: " + cursorPosition);
+	});
+}
--- src/options/options.js
+++ src/options/options.js
@@ -15,6 +15,43 @@ chrome.storage.sync.get(["settings_format"], function(value) {
     $('textarea').trigger('input');
 });
 
+$("#updatesupport").click(function() {
+    updateSupport();
+});
+
+function updateSupport() {
+    $.get("https://lolstatic-a.akamaihd.net/player-support/support.riotgames.com/help-center-data/ticket_form_fields_live.json.gz", function(response) {
+        const forms = response.forms.map(function(f) {
+            return {
+                id: f.id,
+                name: f.raw_display_name.de
+            }
+        });
+
+        chrome.storage.sync.set({settings_support: forms}, function() {
+           alert("Updated forms from support. Number of forms loaded: " + forms.length);
+           updateSupportUI();
+        });
+    });
+}
+
+function updateSupportUI() {
+    $("#supportlist").empty();
+
+    chrome.storage.sync.get(["settings_support"], function(value) {
+        const supportList = value.settings_support;
+
+        if (supportList == null) {
+            // Initial request
+            updateSupport();
+        } else {
+            supportList.forEach(function(form) {
+                $("#supportlist").append("<li>[" + form.id + "]: " + form.name + "</li>");
+            });
+        }
+    });
+}
+
 $("#saveformat").click(function() {
     const format = $("#format").val();
 
@@ -22,3 +59,5 @@ $("#saveformat").click(function() {
         alert("The following format was saved:\n" + format);
     });
 });
+
+updateSupportUI();
